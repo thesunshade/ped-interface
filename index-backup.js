04227295a934ef68876cf12fcf0b1d8ffcdfe04f
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-04-25 11:57:38 +0530

    updates

--- index-backup.js
+++ index-backup.js
@@ -31,21 +31,21 @@ let fuzzyMode = true;
 const inputWord = document.getElementById("word-input");
 inputWord.focus();
 inputWord.addEventListener("input", e => {
-  if (!inputWord.value || inputWord.value.length <= 2) {
+  if (!e.currentTarget.value || e.currentTarget.value.length <= 2) {
     list.innerHTML = "";
-  } else if (inputWord.value && inputWord.value.length > 2) {
+  } else if (e.currentTarget.value && e.currentTarget.value.length > 2) {
     {
       let htmlList = `<ul class="results">`;
       console.log(fuzzyMode);
       if (fuzzyMode === true) {
         ped.forEach((item, index) => {
-          if (new RegExp(fuzzy(inputWord.value), "i").test(fuzzy(item[0]))) {
+          if (new RegExp(fuzzy(e.currentTarget.value), "i").test(fuzzy(item[0]))) {
             htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
           }
         });
       } else if (fuzzyMode === false) {
         ped.forEach((item, index) => {
-          if (new RegExp(inputWord.value, "i").test(item[0])) {
+          if (new RegExp(e.currentTarget.value, "i").test(item[0])) {
             htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
           }
         });
