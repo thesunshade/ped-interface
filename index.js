0f0ed75aef42642161ea2dae633fe0d663b7730e
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-04-25 12:45:02 +0530

    add localStorage

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ import fuzzy from "./fuzzy.js";
 // }
 // buildDic(ped);
 
-let fuzzyMode = true;
+let fuzzyMode;
 
 const inputWord = document.getElementById("word-input");
 inputWord.focus();
@@ -34,48 +34,51 @@ inputWord.addEventListener("input", e => {
   if (!inputWord.value || inputWord.value.length <= 2) {
     list.innerHTML = "";
   } else if (inputWord.value && inputWord.value.length > 2) {
-    {
-      let htmlList = `<ul class="results">`;
-      console.log(fuzzyMode);
-      if (fuzzyMode === true) {
-        ped.forEach((item, index) => {
-          if (new RegExp(fuzzy(inputWord.value), "i").test(fuzzy(item[0]))) {
-            htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
-          }
-        });
-      } else if (fuzzyMode === false) {
-        ped.forEach((item, index) => {
-          if (new RegExp(inputWord.value, "i").test(item[0])) {
-            htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
-          }
-        });
-      }
-      list.innerHTML = htmlList + "</ul>";
+    createResultList();
 
-      const resultList = document.querySelectorAll(".item");
-      resultList.forEach(listItem => {
-        listItem.addEventListener("click", e => {
-          definitionArea.innerHTML = `<h1>${ped[e.currentTarget.id][0]} <a href="https://suttacentral.net/define/${
-            ped[e.currentTarget.id][0]
-          }">🔗</a></h1>${ped[e.currentTarget.id][1]}`;
-          window.scrollTo(0, 0);
-        });
+    const resultList = document.querySelectorAll(".item");
+    resultList.forEach(listItem => {
+      listItem.addEventListener("click", e => {
+        definitionArea.innerHTML = `<h1>${ped[e.currentTarget.id][0]} <a href="https://suttacentral.net/define/${
+          ped[e.currentTarget.id][0]
+        }" title="Go to the entry on SuttaCentral.net" target="_blank">🔗</a></h1>${ped[e.currentTarget.id][1]}`;
+        window.scrollTo(0, 0);
       });
-    }
+    });
   }
 });
 
 const fuzzyBox = document.getElementById("fuzzy-box");
+
+if (localStorage.fuzzyMode === undefined) {
+  localStorage.fuzzyMode === true;
+} else if (localStorage.fuzzyMode === false) {
+  fuzzyBox.checked = false;
+  fuzzyMode = false;
+} else {
+  fuzzyMode = true;
+}
+
 fuzzyBox.addEventListener("click", e => {
-  // console.log(e.originalTarget.checked);
-  // console.log(inputWord.value);
   fuzzyMode = e.originalTarget.checked;
+  localStorage.fuzzyMode = e.originalTarget.checked;
+  createResultList();
 });
 
-// if (localStorage.strictMode) {
-//   if (localStorage.paliToggle === "true") {
-//     // suttaArea.classList.add("hide-pali");
-//   }
-// } else {
-//   localStorage.paliToggle = "false";
-// }
+function createResultList() {
+  let htmlList = `<ul class="results">`;
+  if (fuzzyMode === true) {
+    ped.forEach((item, index) => {
+      if (new RegExp(fuzzy(inputWord.value), "i").test(fuzzy(item[0]))) {
+        htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
+      }
+    });
+  } else if (fuzzyMode === false) {
+    ped.forEach((item, index) => {
+      if (new RegExp(inputWord.value, "i").test(item[0])) {
+        htmlList += `<li class="item" id="${index}">${item[0]}</li>`;
+      }
+    });
+  }
+  list.innerHTML = htmlList + "</ul>";
+}
